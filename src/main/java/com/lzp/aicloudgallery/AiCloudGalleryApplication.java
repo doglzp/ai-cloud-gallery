d904fe627475b47b1c99066d9654a2103d88859f
linzp <52520207@qq.com>
2025-02-28 21:02:49 +0800

    create project

---
+++ src/main/java/com/lzp/aicloudgallery/AiCloudGalleryApplication.java
@@ -0,0 +1,13 @@
+package com.lzp.aicloudgallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCloudGalleryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCloudGalleryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lzp/aicloudgallery/AiCloudGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzp.aicloudgallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCloudGalleryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
